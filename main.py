892be5fb9281acf08764cec5aaf12bb95149502e
Jakemulrooney <91212898+Jakemulrooney@users.noreply.github.com>
2021-11-04 11:29:18 +0000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,15 @@
+def on_button_pressed_a():
+    servos.P1.set_angle(0)
+    basic.show_number(0)
+    basic.pause(500)
+    servos.P1.set_angle(90)
+    basic.show_number(90)
+    basic.pause(500)
+    servos.P1.set_angle(180)
+    basic.show_number(180)
+    basic.pause(500)
+    servos.P1.stop()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+basic.show_string("SEVRVO TIME", 65)
+basic.show_icon(IconNames.HAPPY)
